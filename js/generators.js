a1856d7654652e2ac2f9cdf8d7822a09c62bb25a
z3n1th4l@gmail.com <z3n1th4l@gmail.com>
2023-08-10 21:22:08 +0300

    pwa.js testing #2

--- js/generators.js
+++ js/generators.js
@@ -5,6 +5,7 @@ addLayer("G", {
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
+		power: new Decimal(0),
     }},
     color: "#ffffff",
     requires: new Decimal(1e72), // Can be a function that takes requirement increases into account
@@ -12,7 +13,7 @@ addLayer("G", {
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 2, // Prestige currency exponent
+    exponent: 3, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -20,8 +21,48 @@ addLayer("G", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
+    generatorEffect() {
+     let Base = player.G.power
+     let Power = new Decimal(0.5)
+     let Softcap1threshold = new Decimal(200e12)
+     let Softcap1factor = new Decimal(0.5)
+    
+     let Calculation = new Decimal.pow(Base, Power)
+    
+     if (Calculation.gte(Softcap1threshold)) {
+      Calculation = Calculation.mul(Softcap1threshold).pow(Softcap1factor)
+     }
+     return Calculation
+    },
+    generatePower() {
+     let Base = player.G.points
+     let Power = new Decimal(2.5)
+     
+     let Calculation = new Decimal.pow(Power, Base).sub(1)
+     return Calculation
+    },
+    update(delta) {
+     player.G.power = player.G.power.add((tmp.G.generatePower).times(delta))
+    },
     branches : [["P", "#ffffff", "10"]],
     row: 1, // Row the layer is in on the tree (0 is the first row)
-    
+    tabFormat: {
+    "Generators": {
+      content: [
+      "main-display",
+      "prestige-button",
+      "resource-display",
+      "blank",
+      "blank",
+      ['raw-html', () => {
+          return `
+          <div>
+          You have ${format(player.G.power)} Power, that is generated ${format(tmp.G.generatePower)} / sec<br>
+          Your Power is translated as ${format(tmp.G.generatorEffect)}× boost to lower layer currencies
+          </div>`
+                }],
+                ]
+    }
+    },
     layerShown(){return player.N.points.gte(11)}
 })
